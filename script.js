70d8fe53c42788c9f3922e31c36a482a96291137
vaardhanraju <vaardhanraju@gmail.com>
2024-07-19 21:45:16 +0530

    Submission of RPS with added UI. And a different set of logic to play the game.

--- script.js
+++ script.js
@@ -1,72 +1,109 @@
 function getComputerChoice() {
-    const choice = ["rock" , "paper", "scissors"]
-    let value = Math.floor(Math.random() * 3)
+    const choice = ["rock" , "paper", "scissors","rock" , "paper", "scissors"]
+    let value = Math.floor(Math.random() * 6)
     return choice[value]
 }
-
-function getHumanChoice() {
-    return prompt("Enter your Choice: rock, paper, scissors")
-}
-
 function playRound(humanChoice, computerChoice) {
+    document.getElementById("player-choice").textContent = humanChoice;
+    document.getElementById("robot-choice").textContent = computerChoice;
+
+    const roundStatus = document.getElementById("round-win");
+
     if (humanChoice === computerChoice) {
-        console.log(`Tie! You both choose ${humanChoice}`);
+        roundStatus.textContent = "tie"
     }
     else if (humanChoice === "rock") {
         if (computerChoice === "paper") {
+            roundStatus.textContent = "paper beats rock"
             console.log(`You lose! Computer choose ${computerChoice}`)
-            return computerScore++
+            robotScore++
         }
         else if (computerChoice === "scissors") {
+            roundStatus.textContent = "rock beats scissors"
             console.log(`You won! Computer choose ${computerChoice}`)
-            return humanScore++
+            humanScore++
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "scissors") {
+            roundStatus.textContent = "scissors beats paper"
             console.log(`You lose! Computer choose ${computerChoice}`)
-            return computerScore++
+            robotScore++
         }
         else if (computerChoice === "rock") {
+            roundStatus.textContent = "paper beats rock"
             console.log(`You won! Computer choose ${computerChoice}`)
-            return humanScore++
+            humanScore++
         }
     }
     else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
+            roundStatus.textContent = "rock beats scissors"
             console.log(`You lose! Computer choose ${computerChoice}`)
-            return computerScore++
+            robotScore++
         }
         else if (computerChoice === "paper") {
+            roundStatus.textContent = "paper beats rock"
             console.log(`You won! Computer choose ${computerChoice}`)
-            return humanScore++
+            humanScore++
         }
+    }
+    document.getElementById("player-current-score").textContent = humanScore;
+    document.getElementById("robot-current-score").textContent = robotScore;
+    if (humanScore == 5) {
+        document.getElementById("round-win").textContent = "you win!"
+        return 1
+    }
+    else if (robotScore == 5) {
+        document.getElementById("round-win").textContent = "Robot win!"
+        return 1
     }
 }
 
 
 let humanScore = 0
-let computerScore = 0
-
-function startGame() {
-    for (let i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice().toLowerCase();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }    
-}
+let robotScore = 0
 
 
-startGame(); 
+const rockBtn = document.getElementById("rock");
+rockBtn.addEventListener('click', () => {
+    if (humanScore >= 5 || robotScore >= 5) {
+        console.log("Game over");
+        document.getElementById("round-win").textContent = "Game over"
+    }
+    else {
+        return playRound(rockBtn.id, getComputerChoice());
+    }
+})
 
-if (humanScore > computerScore) {
-    console.log("You won!")
-}
-else if (computerScore > humanScore) {
-    console.log("Computer won")
-}
-else {
-    console.log("Tie")
-}
 
-alert("Open console to see the Winner")
+const paperBtn = document.getElementById("paper");
+paperBtn.addEventListener('click', () => {
+    if (humanScore >= 5 || robotScore >= 5) {
+        console.log("Game over");
+        document.getElementById("round-win").textContent = "Game over"
+    }
+    else {
+        return playRound(rockBtn.id, getComputerChoice());
+    }
+})
+
+
+const scissorsBtn = document.getElementById("scissors");
+scissorsBtn.addEventListener('click', () => {
+    if (humanScore >= 5 || robotScore >= 5) {
+        console.log("Game over"); 
+        document.getElementById("round-win").textContent = "Game over"  
+    }
+    else {
+        return playRound(rockBtn.id, getComputerChoice());
+    }
+})
+
+document.getElementById("restart").addEventListener('click', () => {
+    humanScore = 0;
+    robotScore = 0;
+    document.getElementById("player-current-score").textContent = "0";
+    document.getElementById("robot-current-score").textContent = "0";
+    document.getElementById("round-win").textContent = "? Beats ?" 
+})
